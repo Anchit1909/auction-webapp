53b0a864492ccd7cae858c135016586e3304d7f3
Anchit Sinha <anchit1909@gmail.com>
2023-07-16 09:53:13 +0530

    Minor fix

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import { AuctionBody } from "./components/auctions/Body";
 import { NavComp } from "./components/authentication/NavComp";
 import { AuthProvider } from "./context/AuthContext";
-import Footer from "./components/Footer";
 import StartPage from "./components/StartPage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Footer } from "./components/Footer";
 
 const router = createBrowserRouter([
   {
